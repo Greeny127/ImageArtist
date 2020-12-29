16e1feb7123b6d90926d70fdad3d65195a21952a
Greeny127 <75127118+Greeny127@users.noreply.github.com>
2020-12-29 21:41:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import time
+import cv2
+import pyautogui
+from colormap import rgb2hex
+
+class Paint:
+
+    def __init__(self):
+        self.image = cv2.imread('image.jpg')
+        self.size = self.image.shape
+        self.x = 0
+        self.y = 0
+        self.startcord = [self.x, self.y] #Change this for starting coord
+
+    def setColor(self, hex):
+        pyautogui.click(355, 522, clicks=2) #Change this for the pallete coord
+        pyautogui.write(hex)
+
+    def getPixels(self):
+        rows = self.size[0]
+        cols = self.size[1]
+        pixels = []
+
+        for i in range(rows):
+            for j in range(cols):
+                r, g, b = int(self.image[i, j, 2]), int(self.image[i, j, 1]), int(self.image[i, j, 0]) #RGB Values
+
+                if r >= 250 and g >= 250 and b >= 250: #Checking if it's white, no need to waste time since background is already white
+                    pass
+
+                else:
+                    hexVal = rgb2hex(r, g, b) #Hex Value
+                    pixeldata = (self.startcord[:], hexVal, i, j) #Packing it for appending into 'pixels'
+
+                    pixels.append(pixeldata)
+
+                self.startcord[1] += 1 #Adds 1 to 'startcord' y value, since the pixel is over
+            
+            self.startcord[1] = self.y #Resets the Y value to 'startcord' y value
+            self.startcord[0] += 1 #Adds 1 to 'startcord' x value
+
+        return pixels
+        
+    def draw(self):
+        print("Getting Pixel Data")
+        pixelData = self.getPixels() #Gets all the pixel coordinates and RGB values
+        done = []
+        print("Starting Drawing")
+
+        for pixel in pixelData: #cheacks if all the pixels of a color is done by checking if it's in 'done'
+            if pixel in done:
+                pass
+            
+            else: #If not then it will search for the pixel and color all the pixels and then append to 'done'
+                self.setColor(pixel[1])
+                for nums in pixelData:
+                    if pixel[1] == nums[1]:
+                        done.append(nums)
+                        pyautogui.click(x=nums[0][0], y=nums[0][1])
+                        print(nums[0])
+
+
+brush = Paint() #Creating a Paint() object
+time.sleep(5) #5 seconds to switch to the Paint.net window
+print("Started")
+brush.draw()
+
+print("Done")
